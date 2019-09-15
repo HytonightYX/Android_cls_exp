49757ee705d70191c2b75c3f013fe8b015ea987e
HytonightYX <whosiyuan@qq.com>
2019-09-15 19:57:27 +0800

    gitignore提交删除.idea

---
+++ exp1_android_dev_env/app/src/main/java/com/example/exp1_android_dev_env/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.exp1_android_dev_env;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
